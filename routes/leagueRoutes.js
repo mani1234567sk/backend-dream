ba5aa94d1cf6904055c1bde25f896b370d927349
mani1234567sk <abdulrehmn78976@gmail.com>
2025-08-10 01:09:17 +0500

    Update leagueRoutes.js

--- routes/leagueRoutes.js
+++ routes/leagueRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const leagueController = require('../controllers/leagueController');
-const { authenticateToken } = require('../middleware/authMiddleware');
+const { authenticateToken, isAdmin } = require('../middleware/authMiddleware');
 
 router.get('/', leagueController.getLeagues);
 router.post('/', authenticateToken, leagueController.createLeague);
+router.put('/:id', authenticateToken, isAdmin, leagueController.updateLeague);
 router.post('/:id/join', authenticateToken, leagueController.joinLeague);
 
 module.exports = router;

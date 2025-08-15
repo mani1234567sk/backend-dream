70032a57079a6744c6a0caaba0b343b96c41db7c
mani1234567sk <abdulrehmn78976@gmail.com>
2025-08-15 17:49:32 +0500

    Update teamRoutes.js

--- routes/teamRoutes.js
+++ routes/teamRoutes.js
@@ -3,9 +3,15 @@ const router = express.Router();
 const teamController = require('../controllers/teamController');
 const { authenticateToken, isAdmin } = require('../middleware/authMiddleware');
 
+// Get all teams (public route)
 router.get('/', teamController.getTeams);
+
+// Admin only routes
 router.post('/', authenticateToken, isAdmin, teamController.createTeam);
 router.put('/:id', authenticateToken, isAdmin, teamController.updateTeam);
 router.delete('/:id', authenticateToken, isAdmin, teamController.deleteTeam);
+
+// Team specific routes
+router.get('/:id', teamController.getTeamById);
 
 module.exports = router;

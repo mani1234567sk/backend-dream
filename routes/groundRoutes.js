3c82c3ec8fc24eaec5273de4d1ffad7440396c9b
mani1234567sk <abdulrehmn78976@gmail.com>
2025-08-16 19:59:52 +0500

    Update groundRoutes.js

--- routes/groundRoutes.js
+++ routes/groundRoutes.js
@@ -1,3 +1,4 @@
+// routes/grounds.js
 const express = require('express');
 const router = express.Router();
 const groundController = require('../controllers/groundController');
@@ -5,6 +6,8 @@ const { authenticateToken } = require('../middleware/authMiddleware');
 
 router.get('/', groundController.getGrounds);
 router.post('/', authenticateToken, groundController.createGround);
+router.put('/:id', authenticateToken, groundController.updateGround); // Added for updating grounds
+router.delete('/:id', authenticateToken, groundController.deleteGround); // Added for deleting grounds
 router.get('/:id/reviews', groundController.getReviews);
 router.post('/:id/reviews', authenticateToken, groundController.createReview);
 
